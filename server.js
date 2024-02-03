973ac5fe3ced93bb1515fe95d1db06703a5f37ae
Ilansuli1 <ilansuli2148@gmail.com>
2024-02-03 15:39:02 +0200

    msg

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-
+const axios = require("axios");
 const app = express();
 const http = require("http").createServer(app);
 
@@ -32,6 +32,23 @@ app.use("/api/auth", authRoutes);
 app.use("/api/aisle", aisleRoutes);
 app.use("/api/user", userRoutes);
 
+app.get("/api/test", async (req, res) => {
+  try {
+    res.send("test");
+  } catch (err) {
+    loggerService.error("Failed to get test", err);
+    res.status(500).send({ err: "Failed to get test" });
+  }
+});
+
+setInterval(async () => {
+  try {
+    const response = await axios.get(`https://ganshelshoshanim.com/api/test`);
+    console.log("Request to / successful:", response.data);
+  } catch (error) {
+    console.error("Error making request to /:", error.message);
+  }
+}, 13 * 60 * 1000);
 // Make every server-side-route to match the index.html
 // so when requesting http://localhost:3030/index.html/station/123 it will still respond with
 // our SPA (single page app) (the index.html file) and allow vue/react-router to take it from there
@@ -41,6 +58,7 @@ app.get("/**", (req, res) => {
 
 const logger = require("./services/logger.service");
 const { log } = require("console");
+const { default: axios } = require("axios");
 const port = process.env.PORT || 3030;
 http.listen(port, () => {
   logger.info("Server is running on http://localhost:" + port);
