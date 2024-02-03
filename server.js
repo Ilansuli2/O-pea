1e497d81d19eef3ede06c437362a9fe41b22d2e3
Ilansuli1 <ilansuli2148@gmail.com>
2024-02-03 15:46:36 +0200

    fixed axios err

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const axios = require("axios");
+// const axios = require("axios");
 const app = express();
 const http = require("http").createServer(app);
 
